b9ea7313da38cda45c1f99f935bc97bc6dfeac48
Gustav Andreasson <gustav.andreasson@gmail.com>
2018-09-04 10:10:21 +0200

    Fixed plant clicking

--- static/js/Garden.js
+++ static/js/Garden.js
@@ -2,190 +2,172 @@ var GARDEN_HEIGHT = 1000;
 var GARDEN_WIDTH = 1000;
 
 function Garden() {
-  var self = this;
-  this.moving = false;
-  this.width = GARDEN_WIDTH;
-  this.height = GARDEN_HEIGHT;
-  this.div_width = $(".garden").width();
-  this.div_height = $(".garden").height();
-  if (this.div_width > this.div_height) {
-    this.scale = this.div_width / this.width;
-    this.top_x = 0;
-    this.top_y = (this.height - this.div_height / this.scale) / 2;
-  } else {
-    this.scale = this.div_height / this.height;
-    this.top_y = 0;
-    this.top_x = (this.width - this.div_width / this.scale) / 2;
-  }
+    var self = this;
+    this.moving = false;
+    this.width = GARDEN_WIDTH;
+    this.height = GARDEN_HEIGHT;
+    this.div_width = $(".garden").width();
+    this.div_height = $(".garden").height();
+    if (this.div_width > this.div_height) {
+        this.scale = this.div_width / this.width;
+        this.top_x = 0;
+        this.top_y = (this.height - this.div_height / this.scale) / 2;
+    } else {
+        this.scale = this.div_height / this.height;
+        this.top_y = 0;
+        this.top_x = (this.width - this.div_width / this.scale) / 2;
+    }
 
     this.plants =  {};
     this.species = {};
+    this.open_plant = null;
 
-  this.zoom_in = function() {
-    if (self.width > 40 && self.height > 40) {
-      if (this.div_width > this.div_height) {
-        self.top_x += 20;
-        self.top_y += 20 * (self.div_height / self.div_width);
-      } else {
-        self.top_x += 20 * (self.div_width / self.div_height);
-        self.top_y += 20;
-      }
-      self.width -= 40;
-      self.height -= 40;
-      self.moved();
+    this.zoom_in = function() {
+        if (self.width > 40 && self.height > 40) {
+            if (this.div_width > this.div_height) {
+                self.top_x += 20;
+                self.top_y += 20 * (self.div_height / self.div_width);
+            } else {
+                self.top_x += 20 * (self.div_width / self.div_height);
+                self.top_y += 20;
+            }
+            self.width -= 40;
+            self.height -= 40;
+            self.moved();
+        }
+        return false;
     }
-    return false;
-  }
 
-  this.zoom_out = function() {
-    if (this.div_width > this.div_height) {
-      self.top_x -= 20;
-      self.top_y -= 20 * (self.div_height / self.div_width);
-    } else {
-      self.top_x -= 20 * (self.div_width / self.div_height);
-      self.top_y -= 20;
+    this.zoom_out = function() {
+        if (this.div_width > this.div_height) {
+            self.top_x -= 20;
+            self.top_y -= 20 * (self.div_height / self.div_width);
+        } else {
+            self.top_x -= 20 * (self.div_width / self.div_height);
+            self.top_y -= 20;
+        }
+        self.width += 40;
+        self.height += 40;
+        self.moved();
+        return false;
     }
-    self.width += 40;
-    self.height += 40;
-    self.moved();
-    return false;
-  }
 
-  this.move = function(x, y) {
-    self.top_x += x;
-    self.top_y += y;
-    self.moved();
-  }
-
-  this.to_screen_x = function(x, abs) {
-    var top_x = self.top_x;
-    if (abs) top_x = 0;
-    return (x - top_x) * self.scale;
-  };
-
-  this.to_screen_y = function(y, abs) {
-    var top_y = self.top_y;
-    if (abs) top_y = 0;
-    return (y - top_y) * self.scale;
-  };
-
-  this.to_map_x = function(x, abs) {
-    var top_x = self.top_x;
-    if (abs) top_x = 0;
-    return x / self.scale + top_x;
-  }
-
-  this.to_map_y = function(y, abs) {
-    var top_y = self.top_y;
-    if (abs) top_y = 0;
-    return y / self.scale + top_y;
-  }
-
-  this.moved = function() {
-    self.div_width = $(".garden").width();
-    self.div_height = $(".garden").height();
-    if (self.div_width > self.div_height) {
-      self.scale = self.div_width / self.width;
-    } else {
-      self.scale = self.div_height / self.height;
+    this.move = function(x, y) {
+        self.top_x += x;
+        self.top_y += y;
+        self.moved();
     }
-    $.each(self.plants, function(ix, plant) {
-      plant.position();
-    });
-    //$(".garden .plant").each(self.position_plant);
-    $(".garden").css("background-position", self.to_screen_x(0) + "px " + self.to_screen_y(0) + "px");
-    $(".garden").css("background-size",
-    (self.to_screen_x(GARDEN_WIDTH, true)) + "px " +
-    (self.to_screen_y(GARDEN_HEIGHT, true)) + "px");
-  };
 
-  /*this.position_plant = function() {
-    if (!$(this).hasClass("open")) {
-      this.style.left = (self.to_screen_x($(this).data("coordX")) - 12) + "px";
-      this.style.top = (self.to_screen_y($(this).data("coordY")) - 12) + "px";
-    } else {
-      var left = self.to_screen_x($(this).data("coordX")) - 12;
-      var top = self.to_screen_y($(this).data("coordY")) - 12;
-      var width = $(this).outerWidth();
-      var height = $(this).outerHeight();
-      if (left + width > self.div_width) {
-        this.style.left = (self.div_width - width) + "px";
-      } else if (left < 0) {
-        this.style.left = "0px";
-      } else {
-        this.style.left = left + "px";
-      }
-      if (top + height > self.div_height) {
-        this.style.top = (self.div_height - height) + "px";
-      } else if (top < 0) {
-        this.style.top = "0px";
-      } else {
-        this.style.top = top + "px";
-      }
+    this.to_screen_x = function(x, abs) {
+        var top_x = self.top_x;
+        if (abs) top_x = 0;
+        return (x - top_x) * self.scale;
+    };
+
+    this.to_screen_y = function(y, abs) {
+        var top_y = self.top_y;
+        if (abs) top_y = 0;
+        return (y - top_y) * self.scale;
+    };
+
+    this.to_map_x = function(x, abs) {
+        var top_x = self.top_x;
+        if (abs) top_x = 0;
+        return x / self.scale + top_x;
     }
-  };*/
 
-  this.init_move = function(e) {
-    var pageX, pageY;
-    if (e.type == "touchstart") {
-      pageX = e.originalEvent.touches[0].screenX;
-      pageY = e.originalEvent.touches[0].screenY;
-    } else {
-      if (e.which != 1) {
-        return;
-      }
-      pageX = e.pageX;
-      pageY = e.pageY;
+    this.to_map_y = function(y, abs) {
+        var top_y = self.top_y;
+        if (abs) top_y = 0;
+        return y / self.scale + top_y;
     }
-    $(".garden").on("mousemove touchmove", self.during_move);
-    self.start_move_x = pageX;
-    self.start_move_y = pageY;
-  };
 
-  this.end_move = function(e) {
-    $(".garden").off("mousemove touchmove");
-    setTimeout(function() {
-      self.moving = false;
-    }, 100)
-    return true;
-  };
+    this.moved = function() {
+        self.div_width = $(".garden").width();
+        self.div_height = $(".garden").height();
+        if (self.div_width > self.div_height) {
+            self.scale = self.div_width / self.width;
+        } else {
+            self.scale = self.div_height / self.height;
+        }
+        $.each(self.plants, function(ix, plant) {
+            plant.position();
+        });
+        //$(".garden .plant").each(self.position_plant);
+        $(".garden").css("background-position", self.to_screen_x(0) + "px " + self.to_screen_y(0) + "px");
+        $(".garden").css("background-size",
+        (self.to_screen_x(GARDEN_WIDTH, true)) + "px " +
+        (self.to_screen_y(GARDEN_HEIGHT, true)) + "px");
+    };
 
-  this.during_move = function(e) {
-    var pageX, pageY;
-    if (e.type == "touchmove") {
-      pageX = e.originalEvent.touches[0].screenX;
-      pageY = e.originalEvent.touches[0].screenY;
-    } else {
-      pageX = e.pageX;
-      pageY = e.pageY;
-    }
-    self.moving = true;
-    self.move(self.to_map_x(self.start_move_x - pageX, true), self.to_map_y(self.start_move_y - pageY, true))
-    self.start_move_x = pageX;
-    self.start_move_y = pageY;
-  };
+    this.init_move = function(e) {
+        var pageX, pageY;
+        if (e.type == "touchstart") {
+            pageX = e.originalEvent.touches[0].screenX;
+            pageY = e.originalEvent.touches[0].screenY;
+        } else {
+            if (e.which != 1) {
+                return;
+            }
+            pageX = e.pageX;
+            pageY = e.pageY;
+        }
+        $(".garden").on("mousemove touchmove", self.during_move);
+        self.start_move_x = pageX;
+        self.start_move_y = pageY;
+    };
 
-  this.mapclick = function(e) {
-    if (!self.moving) {
-      if ($(".garden .plant.open").length) {
-        $(".garden .plant.open").removeClass("open").each(self.position_plant);
-      } else {
-        var posX = e.pageX - $(this).offset().left;
-        var posY = e.pageY - $(this).offset().top;
-        $("[name=add_plant] [name=coord_x]").val(self.to_map_x(posX));
-        $("[name=add_plant] [name=coord_y]").val(self.to_map_y(posY));
-        $("[name=add_plant").show();
-      }
-    } else {
-      self.moving = false;
-    }
-  };
+    this.end_move = function(e) {
+        $(".garden").off("mousemove touchmove");
+        setTimeout(function() {
+            self.moving = false;
+        }, 100)
+        return true;
+    };
 
-  this.plantclick = function() {
-    $(".garden .plant.open").removeClass("open").each(self.position_plant);
-    $(this).addClass("open").each(self.position_plant);
-    return false;
-  };
+    this.during_move = function(e) {
+        var pageX, pageY;
+        if (e.type == "touchmove") {
+            pageX = e.originalEvent.touches[0].screenX;
+            pageY = e.originalEvent.touches[0].screenY;
+        } else {
+            pageX = e.pageX;
+            pageY = e.pageY;
+        }
+        self.moving = true;
+        self.move(self.to_map_x(self.start_move_x - pageX, true), self.to_map_y(self.start_move_y - pageY, true))
+        self.start_move_x = pageX;
+        self.start_move_y = pageY;
+    };
+
+    this.mapclick = function(e) {
+        if (!self.moving) {
+            if (self.open_plant) {
+
+                self.plants[self.open_plant].close();
+                self.open_plant = null;
+            } else {
+                var posX = e.pageX - $(this).offset().left;
+                var posY = e.pageY - $(this).offset().top;
+                $("[name=add_plant] [name=coord_x]").val(self.to_map_x(posX));
+                $("[name=add_plant] [name=coord_y]").val(self.to_map_y(posY));
+                $("[name=add_plant").show();
+            }
+        } else {
+            self.moving = false;
+        }
+    };
+
+    this.plantclick = function(id) {
+        if (id != self.open_plant) {
+            if (self.open_plant) {
+                self.plants[self.open_plant].close();
+            }
+            self.open_plant = id;
+            self.plants[id].open();
+        }
+        return false;
+    };
 
     this.load_plants = function() {
         $.getJSON(
@@ -193,7 +175,7 @@ function Garden() {
             {action: "get_plants"},
             function (plants) {
                 $.each(plants, function(plant_id, plant) {
-                    self.plants[plant_id] = new Plant(plant, self);
+                    self.plants[plant.id] = new Plant(plant, self);
                 });
 
                 //$(".garden .plant").each(self.position_plant);
@@ -209,21 +191,22 @@ function Garden() {
             });
     };
 
-  $(".garden").click(self.mapclick);
-  $(".garden .plant").click(self.plantclick);
-  $(".garden").on("mousedown touchstart", self.init_move);
-  $("body").on("mouseup touchend", self.end_move);
-  $("#btn_zoom_in").click(self.zoom_in);
-  $("#btn_zoom_out").click(self.zoom_out);
-  $(".garden").on("wheel", function(e) {
-    if (e.originalEvent.deltaY < 0) {
-      self.zoom_in();
-    } else {
-      self.zoom_out();
-    }
-  });
-  $(window).resize(self.moved);
-  self.moved();
 
-  return this;
+    //$(".garden .plant").click(self.plantclick);
+    $(".garden").click(self.mapclick);
+    $(".garden").on("mousedown touchstart", self.init_move);
+    $("body").on("mouseup touchend", self.end_move);
+    $("#btn_zoom_in").click(self.zoom_in);
+    $("#btn_zoom_out").click(self.zoom_out);
+    $(".garden").on("wheel", function(e) {
+        if (e.originalEvent.deltaY < 0) {
+            self.zoom_in();
+        } else {
+            self.zoom_out();
+        }
+    });
+    $(window).resize(self.moved);
+    self.moved();
+
+    return this;
 }
--- static/js/Plant.js
+++ static/js/Plant.js
@@ -1,6 +1,6 @@
 function Plant(plant, garden) {
     var self = this;
-    this.plant_id = plant.plant_id;
+    this.id = plant.id;
     this.species_id = plant.species_id;
     this.name = plant.name;
     this.description = plant.description;
@@ -8,21 +8,21 @@ function Plant(plant, garden) {
     this.coord_x = plant.coord_x;
     this.coord_y = plant.coord_y;
     this.garden = garden;
-    this.open = false;
+    this.is_open = false;
 
     function add_plant_html() {
-        plant_element = '<div class="plant" id="plant_' + plant.plant_id;
-        plant_element += '" data-plant-id="' + plant.plant_id + '">';
+        plant_element = '<div class="plant" id="plant_' + plant.id;
+        plant_element += '" data-plant-id="' + plant.id + '">';
         plant_element += '<div class="name">' + plant.name + '</div>';
         plant_element += '<div class="description">' + plant.description + '</div>';
         plant_element += '<img src="' + plant.image + '">';
         plant_element += '<div class="data">';
         /*plant_element += '<?php foreach($plant->species->get_data() as $name => $value): ?>';
-          plant_element += '<div class="field">';
-          plant_element += '<span class="name"><?= $name ?></span>';
-          plant_element += '<span class="value"><?= $value ?></span>';
-          plant_element += '</div>';
-          plant_element += '<?php endforeach; ?>';*/
+        plant_element += '<div class="field">';
+        plant_element += '<span class="name"><?= $name ?></span>';
+        plant_element += '<span class="value"><?= $value ?></span>';
+        plant_element += '</div>';
+        plant_element += '<?php endforeach; ?>';*/
         plant_element += '</div>';
         plant_element += '</div>';
 
@@ -30,38 +30,54 @@ function Plant(plant, garden) {
     };
 
     function get_element() {
-      return $("#plant_" + self.plant_id);
+        return $("#plant_" + self.id);
     }
 
     this.position = function() {
-      var element = get_element();
-      if (!self.open) {
-        element.css("left", (self.garden.to_screen_x(self.coord_x) - 12) + "px");
-        element.css("top", (self.garden.to_screen_y(self.coord_y) - 12) + "px");
-      } else {
-        var left = self.garden.to_screen_x(self.coord_x) - 12;
-        var top = self.garden.to_screen_y(self.coord_y) - 12;
-        var width = element.outerWidth();
-        var height = element.outerHeight();
-        if (left + width > self.garden.div_width) {
-          element.css("left", (self.garden.div_width - width) + "px");
-        } else if (left < 0) {
-          element.css("left", "0px");
+        var element = get_element();
+        if (!self.is_open) {
+            element.css("left", (self.garden.to_screen_x(self.coord_x) - 12) + "px");
+            element.css("top", (self.garden.to_screen_y(self.coord_y) - 12) + "px");
         } else {
-          element.css("left", left + "px");
+            var left = self.garden.to_screen_x(self.coord_x) - 12;
+            var top = self.garden.to_screen_y(self.coord_y) - 12;
+            var width = element.outerWidth();
+            var height = element.outerHeight();
+            if (left + width > self.garden.div_width) {
+                element.css("left", (self.garden.div_width - width) + "px");
+            } else if (left < 0) {
+                element.css("left", "0px");
+            } else {
+                element.css("left", left + "px");
+            }
+            if (top + height > self.garden.div_height) {
+                element.css("top", (self.garden.div_height - height) + "px");
+            } else if (top < 0) {
+                element.css("top", "0px");
+            } else {
+                element.css("top", top + "px");
+            }
         }
-        if (top + height > self.garden.div_height) {
-          element.css("top", (self.garden.div_height - height) + "px");
-        } else if (top < 0) {
-          element.css("top", "0px");
-        } else {
-          element.css("top", top + "px");
-        }
-      }
+    }
+
+    this.open = function() {
+        self.is_open = true;
+        get_element().addClass("open");
+        self.position();
+    }
+
+    this.close = function() {
+        self.is_open = false;
+        get_element().removeClass("open");
+        self.position()
     }
 
     add_plant_html();
     self.position();
+    get_element().click(function (e) {
+        self.garden.plantclick(self.id);
+        e.stopPropagation();
+    });
 
     return this;
 }
