6eb11dce22e7fa1e71ea4359be476029d1311243
Gustav Andreasson <gustav.andreasson@gmail.com>
2018-09-25 15:03:18 +0200

    improved deploy script

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avze ssh --exclude install --exclude var --exclude *~ ../* gustav@192.168.1.220:/var/www/html/flowermap/
+rsync -avze ssh --exclude install --exclude var --exclude *~ --exclude .git ../ gustav@192.168.1.220:/var/www/html/flowermap/
